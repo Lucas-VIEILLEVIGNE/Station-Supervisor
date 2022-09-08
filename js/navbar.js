45f5d98c2aaa24373a665956098bee12beced63b
Lucas Vieillevigne <lvieillevigne@normandiewebschool.fr>
2022-09-08 10:16:32 +0200

    burger button ok

--- js/navbar.js
+++ js/navbar.js
@@ -5,16 +5,6 @@ function toggleMenu ()
     burger.addEventListener('click',() => 
     {
         navbar.classList.toggle('show-nav');
-        Footer.classList.toggle('show-nav');
     })
 }
 toggleMenu();
-
-
-$('[data-toggle=tooltip]').tooltip();
-
-$('.hover-animation').hover(
-  function() {
-    $( this ).toggleClass( 'animated rubberBand ' + $( this ).attr('data-hover-color') + '-text' );
-  }
-);
